4b6aea7e605056fed55ea43f40ccda0a8d98fa38
NBKP <bharth.kasyap.518@gmail.com>
2026-01-22 18:19:12 +0530

    few more text changes.

--- src/app/silk-product/silk-product.ts
+++ src/app/silk-product/silk-product.ts
@@ -18,28 +18,28 @@ export class SilkProduct {
       name: 'Golden Kurta',
       image: '/assets/cream_no_sheen-1.jpg',
       description: 'High quality and premium design for professionals.',
-      price: '₹4,999'
+      price: '₹11,999'
     },
     {
       id: 'item-b',
       name: 'Royal Saree',
       image: '/assets/382005_Blue_Latest-Designer-Satin-Silk-Saree-With-Printed-Blouse-Piece-At-Shubhkala-Fashion_RJ-382005_1.webp',
       description: 'Elegant, modern and built with care.',
-      price: '₹3,499'
+      price: '₹23,499'
     },
     {
       id: 'item-c',
-      name: 'Golden Dress',
+      name: 'Green Kurta Suit',
       image: '/assets/0ab0c2e1650ff08490b894825f2ddca1.jpg',
       description: 'Minimalistic touch for your everyday use.',
-      price: '₹2,799'
+      price: '₹20,799'
     },
     {
       id: 'item-d',
-      name: 'Aqua Kurta',
+      name: 'Gold Evening Dress',
       image: '/assets/EDG1710258615.webp',
       description: 'A bold model with futuristic design excellence.',
-      price: '₹3,999'
+      price: '₹30,999'
     }
   ];
 
